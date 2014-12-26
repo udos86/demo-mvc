1ffb9875ddc652c62c9681a07b8bb15e68ff0c4c
udos86 <git@udos86.de>
2014-12-26 14:59:59 +0100

    scaffold files added

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON("package.json"),
+
+	clean : {
+			
+		options: {
+			force: true
+		},
+		
+		remove: {
+			src: ["dist/"]
+		}			
+	},
+
+        cssmin: {
+
+            minify: {
+
+                files: [{
+
+                    expand: true,
+                    cwd: "css/",
+                    src: "*.css",
+                    dest: "dist/css/",
+                    ext: ".min.css"
+                }]
+            }
+        },
+
+        uglify: {
+
+            minify: {
+
+                files: [{
+
+                    expand: true,
+                    cwd: "scripts/",
+                    src: "**/*.js",
+                    dest: "dist/scripts/",
+                    ext: ".min.js"
+                }]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-contrib-cssmin");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+
+    grunt.registerTask("default", ["clean", "cssmin", "uglify"]);
+};
